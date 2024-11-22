308fac167deff385596001209e716e4c1ed93f12
Tú Trần <87356468+TuTran21195@users.noreply.github.com>
2024-11-23 00:10:13 +0700

    Update DBConnection134.java

--- TimkiemMonAn/src/java/dao/DBConnection134.java
+++ TimkiemMonAn/src/java/dao/DBConnection134.java
@@ -1,17 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-/**
- *
- * @author tutran
- */
+
 public class DBConnection134 {
     public static Connection getConnection() throws SQLException, ClassNotFoundException {
         Class.forName("com.mysql.cj.jdbc.Driver");
